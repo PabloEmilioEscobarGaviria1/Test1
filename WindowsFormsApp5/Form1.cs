2dd45c3c788a765aec969600df908bc56174959a
PabloEmilioEscobarGaviria1 <svinetsof@gmail.com>
2024-02-10 10:09:44 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class MyGenericClass<T>
+        {
+            private T genericMember;
+
+            public MyGenericClass(T value)
+            {
+                genericMember = value;
+            }
+
+            public string DisplayInformation()
+            {
+                return $"Тип: {typeof(T)}, Значение: {genericMember}";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var stringInstance = new MyGenericClass<string>("Привет, мир!");
+            var intInstance = new MyGenericClass<int>(12345);
+
+            MessageBox.Show(stringInstance.DisplayInformation());
+            MessageBox.Show(intInstance.DisplayInformation());
+        }
+    }
+}
+
